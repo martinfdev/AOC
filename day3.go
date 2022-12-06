6494741641a10397eabf5cbffa1ef418a36cc081
pedro <pedromartinf07@gmail.com>
2022-12-05 19:30:23 -0600

    AoCday3 part2

--- day3.go
+++ day3.go
@@ -37,4 +37,31 @@ func day3(data_string string) {
 		total_result += calc_priority(priorities, rucksack)
 	}
 	fmt.Println(total_result)
+	part2day3(rucksacks, priorities)
+}
+
+func part2day3(rucksaks []string, mapValue map[rune]int) {
+	//anonymous function to calc_priority
+	calc_priority := func(priorities_maping map[rune]int, rucksack1 string, rucksack2 string, rucksack3 string) int {
+		for i := 0; i < len(rucksack1); i++ {
+			c := rune(rucksack1[i])
+			if i < len(rucksack1) && strings.ContainsRune(rucksack2, c) && strings.ContainsRune(rucksack3, c) ||
+				i >= len(rucksack1) && strings.ContainsRune(rucksack2, c) && strings.ContainsRune(rucksack3, c) {
+				return priorities_maping[c]
+			}
+		}
+		return 0
+	}
+
+	count := 0
+	valueResult := 0
+
+	for {
+		valueResult += calc_priority(mapValue, rucksaks[count], rucksaks[count+1], rucksaks[count+2])
+		count += 3
+		if count >= len(rucksaks) {
+			break
+		}
+	}
+	fmt.Println(valueResult)
 }
