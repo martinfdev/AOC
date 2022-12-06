429f37a7024dd08959fa6862118b656d3e7c323a
pedro <pedromartinf07@gmail.com>
2022-12-06 00:32:03 -0600

    AoCday4 part1

---
+++ day4.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func day4(input_data string) {
+	tmp_input := strings.ReplaceAll(input_data, "\r", "")
+	pairs := strings.Split(tmp_input, "\n")
+
+	count := 0
+
+	for _, pair := range pairs {
+		asigments := strings.Split(pair, ",")
+		first := strings.Split(asigments[0], "-")
+		second := strings.Split(asigments[1], "-")
+
+		first_start, _ := strconv.Atoi(first[0])
+		first_end, _ := strconv.Atoi(first[1])
+
+		second_start, _ := strconv.Atoi(second[0])
+		second_end, _ := strconv.Atoi(second[1])
+
+		if first_start <= second_start && first_end >= second_end ||
+			second_start <= first_start && second_end >= first_end {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	// day1(read_file("./files/day1.txt"))
 	// day2(read_file("./files/day2.txt"))
-	day3(read_file("./files/day3.txt"))
+	// day3(read_file("./files/day3.txt"))
+	day4(read_file("./files/day4.txt"))
 }
