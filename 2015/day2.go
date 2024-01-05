fb13771e8571df000533e72444196b555511b8fa
pedro <pedromartinf07@gmail.com>
2024-01-04 21:57:07 -0600

    day2 part1

---
+++ 2015/day2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Day2() {
+	data := Read_file("files/day2.txt")
+	data_split := strings.Split(data, "\n")
+
+	total_paper := 0
+	for i, v := range data_split {
+		l_w_h := strings.Split(v, "x")
+		//cast string to int
+		to_int := func(s string) int {
+			num, err := strconv.Atoi(s)
+			if err != nil {
+				fmt.Println(err)
+			}
+			return num
+		}
+
+		dimension := make([]int, len(l_w_h))
+		for i, v := range l_w_h {
+			dimension[i] = to_int(v)
+		}
+		if len(l_w_h) == 3 {
+			l, w, h := dimension[0], dimension[1], dimension[2]
+			total_paper += (2 * l * w) + (2 * w * h) + (2 * h * l) + min(l*w, w*h, h*l)
+		} else {
+			fmt.Println("Error: ", i, v)
+		}
+	}
+	fmt.Println("Total paper: ", total_paper)
+}
