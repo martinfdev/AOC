f07152977d70d2d5d27d278adba7ec3e33bf5727
pedro <pedromartinf07@gmail.com>
2024-01-04 19:01:27 -0600

    fixing repo

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ func main() {
 	// day1(read_file("./files/day1.txt"))
 	// day2(read_file("./files/day2.txt"))
 	// day3(read_file("./files/day3.txt"))
-	day4(read_file("./files/day4.txt"))
+	// day4(read_file("./files/day4.txt"))
+	day5(read_file("./files/day5.txt"))
 }
