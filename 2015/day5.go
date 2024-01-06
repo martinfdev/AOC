416be42517f59c3472f68db904eae61a02c046b0
pedro <pedromartinf07@gmail.com>
2024-01-06 14:00:01 -0600

    part two

--- 2015/day5.go
+++ 2015/day5.go
@@ -19,6 +19,7 @@ func Day5() {
 		}
 	}
 	fmt.Println("Nice strings:", nice_strings)
+	part2(data)
 }
 
 func has_three_vowels(data string) bool {
@@ -47,6 +48,40 @@ func has_bad_strings(data string) bool {
 	bad_strings := []string{"ab", "cd", "pq", "xy"}
 	for _, v := range bad_strings {
 		if strings.Contains(data, v) {
+			return true
+		}
+	}
+	return false
+}
+
+// Part 2 new rules
+
+//It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+//It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
+func part2(data string) {
+	var nice_strings int
+	list_data := strings.Split(data, "\n")
+	for _, data_string := range list_data {
+		if has_double_letterp_2(data_string) && has_double_pair(data_string) {
+			nice_strings++
+		}
+	}
+	fmt.Println("2 Nice strings:", nice_strings)
+}
+
+func has_double_pair(data string) bool {
+	for i := 0; i < len(data)-1; i++ {
+		if strings.Contains(data[i+2:], data[i:i+2]) {
+			return true
+		}
+	}
+	return false
+}
+
+func has_double_letterp_2(data string) bool {
+	for i := 0; i < len(data)-2; i++ {
+		if data[i] == data[i+2] {
 			return true
 		}
 	}
