7e9484a2a625efd39d05dbc8ba6ea6b795e64c7e
pedro <pedromartinf07@gmail.com>
2024-01-05 22:45:34 -0600

    day3 part2

--- 2015/day3.go
+++ 2015/day3.go
@@ -5,28 +5,59 @@ import "fmt"
 func Day3() {
 	x := 0 // x coordinate
 	y := 0 // y coordinate
+	robot_x := 0
+	robot_y := 0
+	robo_santa := false
 	data := Read_file("files/day3.txt")
 	houses := make(map[string]int)
-	houses_gifts := 1
 	houses["0,0"] = 1
-	for _, c := range data {
+	for i, c := range data {
+		if i%2 == 0 {
+			robo_santa = false
+		} else {
+			robo_santa = true
+		}
 		switch c {
 		case '^':
-			y++
+			if robo_santa {
+				robot_y++
+			} else {
+				y++
+			}
 		case 'v':
-			y--
+			if robo_santa {
+				robot_y--
+			} else {
+				y--
+			}
 		case '>':
-			x++
+			if robo_santa {
+				robot_x++
+			} else {
+				x++
+			}
 		case '<':
-			x--
+			if robo_santa {
+				robot_x--
+			} else {
+				x--
+			}
 		}
-		coord := fmt.Sprintf("%d,%d", x, y)
-		if _, ok := houses[coord]; !ok {
-			houses[coord] = 1
-			houses_gifts++
+		if robo_santa {
+			_, ok := houses[fmt.Sprintf("%d,%d", robot_x, robot_y)]
+			if ok {
+				houses[fmt.Sprintf("%d,%d", robot_x, robot_y)]++
+			} else {
+				houses[fmt.Sprintf("%d,%d", robot_x, robot_y)] = 1
+			}
+		} else {
+			_, ok := houses[fmt.Sprintf("%d,%d", x, y)]
+			if ok {
+				houses[fmt.Sprintf("%d,%d", x, y)]++
+			} else {
+				houses[fmt.Sprintf("%d,%d", x, y)] = 1
+			}
 		}
 	}
-	fmt.Printf("Santa visited %d houses\n", len(houses))
-
-	println(houses_gifts)
+	fmt.Printf("Santa and robot_stanta total visited %d houses\n", len(houses))
 }
