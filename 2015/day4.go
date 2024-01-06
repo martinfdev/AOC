7d1d8fff0e51a24dd2833d921262aca06e5315f7
pedro <pedromartinf07@gmail.com>
2024-01-05 23:13:29 -0600

    part two

--- 2015/day4.go
+++ 2015/day4.go
@@ -8,14 +8,14 @@ import (
 func Day4() {
 	data := Read_file("files/day4.txt")
 	result := md5_hash(data)
-	fmt.Println("The lowest positive number that produces a hash with 5 leading zeroes is", result)
+	fmt.Println("The lowest positive number that produces a hash with 6 leading zeroes is", result)
 }
 
 func md5_hash(data string) int {
 	var i int
 	for i = 0; ; i++ {
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(data+fmt.Sprintf("%d", i))))
-		if hash[0:5] == "00000" {
+		if hash[0:6] == "000000" {
 			break
 		}
 	}
