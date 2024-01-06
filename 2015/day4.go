09ac4ccf64ae122fafca8d920dcf535412b0707c
pedro <pedromartinf07@gmail.com>
2024-01-05 23:11:21 -0600

    day4 part1

---
+++ 2015/day4.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func Day4() {
+	data := Read_file("files/day4.txt")
+	result := md5_hash(data)
+	fmt.Println("The lowest positive number that produces a hash with 5 leading zeroes is", result)
+}
+
+func md5_hash(data string) int {
+	var i int
+	for i = 0; ; i++ {
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(data+fmt.Sprintf("%d", i))))
+		if hash[0:5] == "00000" {
+			break
+		}
+	}
+	return i
+}
