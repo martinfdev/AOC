db3993418e5d692a285675a6e6a4302000e5abb7
pedro <pedromartinf07@gmail.com>
2024-01-09 19:25:18 -0600

    fixing gates value

--- 2015/day7.go
+++ 2015/day7.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,44 +11,29 @@ type Gate interface {
 	Signal() uint16
 }
 
-type Wire struct {
-	signal uint16
-}
-
 type Circuit struct {
 	signals map[string]uint16
 	gates   map[string]Gate
 }
 
-type AndGate struct {
-	input1, input2 Gate
+type Wire struct {
+	signal uint16
 }
 
-type NotGate struct {
-	input Gate
-}
-
-func (w *Wire) SetSignal(s uint16) {
-	w.signal = s
+type Not struct {
+	signal uint16
 }
 
 func (w *Wire) Signal() uint16 {
 	return w.signal
 }
 
-func (g *AndGate) Signal() uint16 {
-	return g.input1.Signal() & g.input2.Signal()
-}
-
-func (g *NotGate) Signal() uint16 {
-	return g.input.Signal()
+func (n *Not) Signal() uint16 {
+	return ^n.signal
 }
 
 func proccesInstructions(instructions string) Circuit {
-	circuit := Circuit{
-		signals: make(map[string]uint16),
-		gates:   make(map[string]Gate),
-	}
+	circuit := Circuit{make(map[string]uint16), make(map[string]Gate)}
 	scanner := bufio.NewScanner(strings.NewReader(instructions))
 	for scanner.Scan() {
 		instruction := scanner.Text()
@@ -60,21 +46,27 @@ func proccesInstruction(circuit *Circuit, instruction string) {
 	parts := strings.Fields(instruction)
 	switch len(parts) {
 	case 3:
-		// 123 -> x
+		// assignment
 		signal, err := strconv.ParseUint(parts[0], 10, 16)
 		if err != nil {
-			circuit.gates[parts[2]] = &Wire{}
+			circuit.gates[parts[2]] = &Wire{circuit.signals[parts[0]]}
 		} else {
 			circuit.signals[parts[2]] = uint16(signal)
 		}
 	case 4:
-		// NOT e -> f
-		gate := &NotGate{input: parts[1]}
-		circuit.gates[parts[3]] = gate
+		// NOT
+		circuit.gates[parts[3]] = &Not{circuit.signals[parts[1]]}
+		circuit.signals[parts[3]] = circuit.gates[parts[3]].Signal()
+	case 5:
+		// AND, OR, LSHIFT, RSHIFT
+
 	}
 }
 
 func Day7() {
 	data := Read_file("files/day7.txt")
+	circuit := proccesInstructions(data)
+	final_signal := circuit.signals["i"]
+	fmt.Println(final_signal)
 
 }
