ab599a1504c036fae7fc307a379f6792078acf5e
pedro <pedromartinf07@gmail.com>
2024-01-10 23:37:11 -0600

    part two

--- 2015/day8.go
+++ 2015/day8.go
@@ -9,6 +9,7 @@ func Day8() {
 	input := Read_file("files/day8.txt")
 	data_list := strings.Split(input, "\n")
 	println("part1: ", count_char(data_list))
+	println("part2: ", encoded_char(data_list))
 }
 
 func count_char(data []string) int {
@@ -27,4 +28,25 @@ func count_char(data []string) int {
 		memory += len(line) - 2
 	}
 	return literals - memory
+}
+
+func encoded_char(data []string) int {
+	literals := 0
+	encoded := 0
+
+	for _, line := range data {
+		literals += len(line)
+		encoded += 2
+		for _, char := range line {
+			switch char {
+			case '"':
+				encoded += 2
+			case '\\':
+				encoded += 2
+			default:
+				encoded += 1
+			}
+		}
+	}
+	return encoded - literals
 }
