d6a3031263ed3e5122be536c13abe1d6827068cc
Pedro Martin <pedromartinf07@gmail.com>
2024-10-01 23:31:08 -0600

    upate algoritm to forzce brute to route short

--- 2015/day9.go
+++ 2015/day9.go
@@ -2,58 +2,95 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
-type Node struct {
-	name     string
-	distance int
-}
-
-type Graph struct {
-	nodes map[string]*Node
-	arcs  map[Node]map[*Node]int
-}
-
-type priorityQueue []*Node
-
-func (pq priorityQueue) Len() int {
-	return len(pq)
-}
-
-func (pq priorityQueue) Less(i, j int) bool {
-	return pq[i].distance < pq[j].distance
-}
-
-func (pq priorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq *priorityQueue) Push(x interface{}) {
-	*pq = append(*pq, x.(*Node))
-}
-
-func (pq *priorityQueue) Pop() interface{} {
-	n := len(*pq)
-	node := (*pq)[n-1]
-	*pq = (*pq)[0 : n-1]
-	return node
-}
-
-func generateGraph(nodes *[]Node, data_arr []string) {
-
-}
-
-// dijkstra algorithm
-func shortesrPath(nodes []Node) int {
-	return 0
-}
-
 func Day9() {
 	data := Read_file("files/day9.txt")
-	data_arr := strings.Split(data, "\n")
-	nodes := make([]Node, 0)
-	generateGraph(&nodes, data_arr)
-	fmt.Println(shortesrPath(nodes))
 
+	//regular expression to extract city names and distances
+	re := regexp.MustCompile(`(\w+) to (\w+) = (\d+)`)
+
+	//map to store the distances between cities
+	distances := make(map[string]map[string]int)
+
+	//aplit data into lines
+	lines := strings.Split(data, "\n")
+
+	for _, line := range lines {
+		//extract city names and distances
+		matches := re.FindStringSubmatch(line)
+		if len(matches) == 4 {
+			city1 := matches[1]
+			city2 := matches[2]
+			distance, _ := strconv.Atoi(matches[3])
+
+			if distances[city1] == nil {
+				distances[city1] = make(map[string]int)
+			}
+			if distances[city2] == nil {
+				distances[city2] = make(map[string]int)
+			}
+			distances[city1][city2] = distance
+			distances[city2][city1] = distance
+		}
+	}
+
+	//get list of cities
+	cities := make([]string, 0, len(distances))
+	for city := range distances {
+		cities = append(cities, city)
+	}
+
+	//found the route with the shortest distance
+	rute, distance := findShortestRoute(cities, distances)
+	fmt.Println("Day 9: ", distance, rute)
+}
+
+func findShortestRoute(cities []string, distances map[string]map[string]int) ([]string, int) {
+	//initialize variables
+	shortestDistance := 0
+	shortestRoute := make([]string, 0)
+
+	//get all possible routes
+	routes := permutations(cities)
+
+	//iterate over all routes
+	for _, route := range routes {
+		distance := 0
+		for i := 0; i < len(route)-1; i++ {
+			distance += distances[route[i]][route[i+1]]
+		}
+		if shortestDistance == 0 || distance < shortestDistance {
+			shortestDistance = distance
+			shortestRoute = route
+		}
+	}
+	return shortestRoute, shortestDistance
+}
+
+func permutations(cities []string) [][]string {
+	//initialize variables
+	perm := make([][]string, 0)
+	used := make(map[string]bool)
+
+	//recursive function to get all possible routes
+	var permute func([]string)
+	permute = func(arr []string) {
+		if len(arr) == len(cities) {
+			perm = append(perm, append([]string(nil), arr...))
+			return
+		}
+		for _, city := range cities {
+			if !used[city] {
+				used[city] = true
+				permute(append(arr, city))
+				used[city] = false
+			}
+		}
+	}
+	permute([]string{})
+	return perm
 }
