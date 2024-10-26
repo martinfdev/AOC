5f674e94a7b0419f6254b744f3bb0e52a789ba6f
Pedro <2574151141308@ingenieria.usac.edu.gt>
2024-10-25 21:50:00 -0600

    day17 part2

--- 2015/day17.go
+++ 2015/day17.go
@@ -14,6 +14,10 @@ func Day17() {
 	const targetVolume = 150
 	ways := countWaysDP(containers, targetVolume)
 	fmt.Printf("Day 17 - Part 1: Number of ways to fill %d liters: %d\n", targetVolume, ways)
+
+	// Part 2
+	minK, count := countMinContainerWays(containers, targetVolume)
+	fmt.Printf("Day 17 - Part 2: Minimum number of containers: %d, Number of ways using minimum containers: %d\n", minK, count)
 }
 
 func parseContainers(input string) []int {
@@ -36,4 +40,29 @@ func countWaysDP(capacities []int, target int) int {
 		}
 	}
 	return ways[target]
+}
+
+// how many combinations use the minimum number of containers to reach the target volume
+func countMinContainerWays(capacities []int, target int) (minK int, count int) {
+	n := len(capacities)
+	waysByK := make([][]int, n+1)
+	for k := 0; k <= n; k++ {
+		waysByK[k] = make([]int, target+1)
+	}
+	waysByK[0][0] = 1 // one way to reach volume 0 with 0 containers
+
+	for _, cap := range capacities {
+		for k := n - 1; k >= 0; k-- {
+			for s := target; s >= cap; s-- {
+				waysByK[k+1][s] += waysByK[k][s-cap]
+			}
+		}
+	}
+
+	for k := 1; k <= n; k++ {
+		if waysByK[k][target] > 0 {
+			return k, waysByK[k][target]
+		}
+	}
+	return 0, 0
 }
