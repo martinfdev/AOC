dc92641682429ebf68d533fec5b48989ac28f915
Pedro <2574151141308@ingenieria.usac.edu.gt>
2025-12-01 20:40:25 -0600

    day1 part2 2025

--- 2025/day1.go
+++ 2025/day1.go
@@ -10,6 +10,7 @@ func Day1() {
 	content := read_file("./files/day1.txt")
 	directions := parse_directions(content)
 	fmt.Println(day1_part1(directions, 50))
+	fmt.Println(day1Part2(directions, 50))
 }
 
 type Direction struct {
@@ -55,4 +56,56 @@ func day1_part1(directions []Direction, initpoint int) int {
 		}
 	}
 	return count
+}
+
+func applyRotation(pos int, distance int, dir int) (newPos int, zeroHits int) {
+	if distance <= 0 {
+		return pos, 0
+	}
+
+	var t0 int
+	if dir == 1 {
+		if pos == 0 {
+			t0 = 100
+		} else {
+			t0 = 100 - pos
+		}
+	} else {
+		if pos == 0 {
+			t0 = 100
+		} else {
+			t0 = pos
+		}
+	}
+
+	zeroHits = 0
+	if t0 <= distance {
+		zeroHits = 1 + (distance-t0)/100
+	}
+
+	delta := distance % 100
+	newPos = pos + dir*delta
+	newPos %= 100
+	if newPos < 0 {
+		newPos += 100
+	}
+	return newPos, zeroHits
+}
+
+func day1Part2(directions []Direction, initpoint int) int {
+	pos := initpoint
+	totalZeros := 0
+
+	for _, direction := range directions {
+		if direction.L != -1 {
+			var zeros int
+			pos, zeros = applyRotation(pos, direction.L, -1)
+			totalZeros += zeros
+		} else if direction.R != -1 {
+			var zeros int
+			pos, zeros = applyRotation(pos, direction.R, 1)
+			totalZeros += zeros
+		}
+	}
+	return totalZeros
 }
