1a838d5ce0f031f3b1ae2d9646d4c84bea4c92c7
Pedro <2574151141308@ingenieria.usac.edu.gt>
2025-12-03 22:15:00 -0600

    day3 part2

--- 2025/day3.go
+++ 2025/day3.go
@@ -10,6 +10,9 @@ func Day3() {
 
 	total := maxJoltageBank(content)
 	fmt.Println("Day 3 Max joltage:", total)
+	k := 12
+	total = maxJoltageK(content, k)
+	fmt.Println("Day 3 Max joltage with k =", k, ":", total)
 }
 
 func maxJoltageBank(input string) int {
@@ -35,6 +38,54 @@ func maxJoltageBank(input string) int {
 			}
 		}
 		total += best
+	}
+	return total
+}
+
+// part 2 with optimization algorithm and k number
+func maxJoltageK(input string, k int) int {
+	total := 0
+
+	digitsBuf := make([]byte, 0, 100)
+	stack := make([]byte, 0, 100)
+
+	for i := 0; i < len(input); i++ {
+		b := input[i]
+		if b >= '0' && b <= '9' {
+			digitsBuf = append(digitsBuf, b)
+		}
+
+		isEnd := (i == len(input)-1)
+		if b == '\n' || isEnd {
+
+			if len(digitsBuf) < k {
+				digitsBuf = digitsBuf[:0]
+				continue
+			}
+
+			toDrop := len(digitsBuf) - k
+			stack = stack[:0]
+
+			for _, digit := range digitsBuf {
+
+				for toDrop > 0 && len(stack) > 0 && digit > stack[len(stack)-1] {
+					stack = stack[:len(stack)-1] // pop
+					toDrop--
+				}
+				stack = append(stack, digit) // push
+			}
+
+			stack = stack[:k] // keep only k digits
+
+			lineVal := 0
+			for _, dByte := range stack {
+				lineVal = lineVal*10 + int(dByte-'0')
+			}
+			total += lineVal
+
+			digitsBuf = digitsBuf[:0] // reset for next line
+
+		}
 	}
 	return total
 }
